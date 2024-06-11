8e07178f97a2775dc898d2e84653f96aaad05e4b
inferno0230 <mail@inferno0230.in>
2024-06-11 23:00:25 +0530

    soong: Add TARGET_USES_OPLUS_TOUCH
    Change-Id: I65a51476d1ff7605b2c0df60315093aa3a51439a
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -54,6 +54,7 @@ SOONG_CONFIG_customGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_oplus_camera \
+    uses_oplus_touch \
     uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
@@ -80,6 +81,7 @@ SOONG_CONFIG_customGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALL
 SOONG_CONFIG_customGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_customGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_customGlobalVars_uses_oplus_touch := $(TARGET_USES_OPLUS_TOUCH)
 SOONG_CONFIG_customGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
